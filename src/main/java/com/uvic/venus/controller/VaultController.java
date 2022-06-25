f84404fb80ffcacf2aa7af564a4a7f2d94895998
BrodyGimson <brody.gimson@gmail.com>
2022-06-24 18:40:32 -0700

    Draft methods for vault controller

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -1,4 +1,50 @@
 package com.uvic.venus.controller;
 
+import javax.sql.DataSource;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.uvic.venus.model.SecretEntry;
+import com.uvic.venus.repository.SecretDAO;
+
+@RestController
 public class VaultController {
+    @Autowired
+    SecretDAO secretDAO;
+
+    @Autowired
+    DataSource dataSource;
+
+    @RequestMapping(value="/fetchsecrets", method = RequestMethod.GET)
+    public ResponseEntity<?> fetchSecrets() throws Exception{
+    
+    }
+
+    @RequestMapping(value="/createsecret", method = RequestMethod.POST)
+    public ResponseEntity<?> createSecret(@RequestBody SecretEntry secret) throws Exception{
+    
+    }
+
+    @RequestMapping(value="/readsecret", method = RequestMethod.GET)
+    public ResponseEntity<?> readSecret(@RequestParam int secretID) throws Exception{
+    
+    }
+
+    @RequestMapping(value="/upatesecret", method = RequestMethod.POST)
+    public ResponseEntity<?> updateSecret(@RequestBody SecretEntry secret) throws Exception{
+    
+    }
+
+    @RequestMapping(value="/deletesecret", method = RequestMethod.GET)
+    public ResponseEntity<?> deleteSecret(@RequestParam int secretID) throws Exception{
+    
+    }
 }
