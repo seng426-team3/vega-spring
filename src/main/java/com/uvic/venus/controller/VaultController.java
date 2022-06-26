b340966b5c6d4ddcccb2ce2ae242635e99618253
BrodyGimson <brody.gimson@gmail.com>
2022-06-26 16:36:58 -0700

    Added Request param injector

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -25,18 +25,18 @@ public class VaultController {
     JwtUtil jwtUtil;
 
     @RequestMapping(value="/fetchsecrets", method = RequestMethod.GET)
-    public ResponseEntity<?> fetchSecrets(String token){
+    public ResponseEntity<?> fetchSecrets(@RequestParam String token){
+
         String username = jwtUtil.extractUsername(token);
 
         List<SecretEntry> userSecrets= secretDAO.findByUsername(username);
-        
         
         return ResponseEntity.ok(userSecrets);
     }
 
     @RequestMapping(value="/createsecret", method = RequestMethod.POST)
     public ResponseEntity<?> createSecret(@RequestBody String secretName){
-        SecretEntry newSecretEntry = new SecretEntry("testuser@venus.com", secretName);
+        SecretEntry newSecretEntry = new SecretEntry("jonoliver@venus.com", secretName);
 
         secretDAO.save(newSecretEntry);
 
@@ -46,13 +46,13 @@ public class VaultController {
     @RequestMapping(value="/readsecret", method = RequestMethod.GET)
     public ResponseEntity<?> readSecret(@RequestParam String secretID){
         // TO DO: Implement user secret reading
-        return ResponseEntity.ok("Function not implemented");
+        return ResponseEntity.ok("Function not implemented yet");
     }
 
     @RequestMapping(value="/upatesecret", method = RequestMethod.POST)
     public ResponseEntity<?> updateSecret(@RequestBody SecretEntry secret){
         // TO DO: Implement user secret updating
-        return ResponseEntity.ok("Function not implemented");
+        return ResponseEntity.ok("Function not implemented yet");
     }
 
     @RequestMapping(value="/deletesecret", method = RequestMethod.GET)
