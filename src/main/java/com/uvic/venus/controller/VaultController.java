25a47c74c9f57b8e0638448c8f05c1fd3a3555f8
BrodyGimson <brody.gimson@gmail.com>
2022-06-25 18:35:51 -0700

    Remove unnecessary dependencies

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -3,13 +3,11 @@ package com.uvic.venus.controller;
 import javax.sql.DataSource;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.uvic.venus.model.SecretEntry;
