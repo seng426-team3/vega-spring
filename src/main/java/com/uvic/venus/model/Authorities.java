0ef07dfe50400f9788e79fea12737a73acb6866c
Anton Nikitenko <anikitenko@uvic.ca>
2022-06-25 21:21:52 -0700

    Add role to userinfoCollection so data visible in vega-web

---
+++ src/main/java/com/uvic/venus/model/Authorities.java
@@ -0,0 +1,47 @@
+package com.uvic.venus.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="authorities")
+public class Authorities {
+    
+    @Id
+    private String username;
+    private String authority;
+
+    public Authorities(String username, String authority) {
+        this.username = username;
+        this.authority = authority;
+    }
+
+    public Authorities() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String toString() {
+        return "Authorities{" +
+                "username='" + username + '\'' +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
+}
