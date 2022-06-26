f6fa6c60f0f9239453d732df76b47ce49f6de008
BrodyGimson <brody.gimson@gmail.com>
2022-06-25 19:31:58 -0700

    Added method returns to  vault controller

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -1,5 +1,7 @@
 package com.uvic.venus.controller;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -24,31 +26,38 @@ public class VaultController {
 
     @RequestMapping(value="/userfetchsecrets", method = RequestMethod.GET)
     public ResponseEntity<?> fetchSecrets() throws Exception{
-    
+        // TO DO: Implement user secret fetching
+        return ResponseEntity.ok("Function not implemented");
     }
 
     @RequestMapping(value="/fetchallsecrets", method = RequestMethod.GET)
     public ResponseEntity<?> fetchAllSecrets() throws Exception{
-    
+        List<SecretEntry> secretEntryList = secretDAO.findAll();
+
+        return ResponseEntity.ok(secretEntryList);
     }
 
     @RequestMapping(value="/usercreatesecret", method = RequestMethod.POST)
     public ResponseEntity<?> createSecret(@RequestBody SecretEntry secret) throws Exception{
-    
+        // TO DO: Implement user secret creation
+        return ResponseEntity.ok("Function not implemented");
     }
 
     @RequestMapping(value="/userreadsecret", method = RequestMethod.GET)
     public ResponseEntity<?> readSecret(@RequestParam int secretID) throws Exception{
-    
+        // TO DO: Implement user secret reading
+        return ResponseEntity.ok("Function not implemented");
     }
 
     @RequestMapping(value="/userupatesecret", method = RequestMethod.POST)
     public ResponseEntity<?> updateSecret(@RequestBody SecretEntry secret) throws Exception{
-    
+        // TO DO: Implement user secret updating
+        return ResponseEntity.ok("Function not implemented");
     }
 
     @RequestMapping(value="/userdeletesecret", method = RequestMethod.GET)
     public ResponseEntity<?> deleteSecret(@RequestParam int secretID) throws Exception{
-    
+        // TO DO: Implement user secret deletion
+        return ResponseEntity.ok("Function not implemented");
     }
 }
