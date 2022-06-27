ac6ecf5692e3d16b746387f2a1cf5e96eca8e3a4
BrodyGimson <brody.gimson@gmail.com>
2022-06-26 20:16:06 -0700

    Added file handling to CRUD functions

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -1,14 +1,16 @@
 package com.uvic.venus.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.FileInputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 
 import com.uvic.venus.auth.JwtUtil;
@@ -34,10 +36,12 @@ public class VaultController {
     }
 
     @RequestMapping(value="/createsecret", method = RequestMethod.POST)
-    public ResponseEntity<?> createSecret(@RequestParam String secretName, String token){
+    public ResponseEntity<?> createSecret(@RequestParam String secretName, String token, MultipartFile file) throws Exception{
         String username = jwtUtil.extractUsername(token);
 
-        SecretEntry newSecretEntry = new SecretEntry(username, secretName);
+        FileInputStream secretData = (FileInputStream) file.getInputStream();
+
+        SecretEntry newSecretEntry = new SecretEntry(username, secretName, secretData);
 
         secretDAO.save(newSecretEntry);
 
@@ -45,15 +49,36 @@ public class VaultController {
     }
 
     @RequestMapping(value="/readsecret", method = RequestMethod.GET)
-    public ResponseEntity<?> readSecret(@RequestParam String secretID){
-        // TO DO: Implement user secret reading
-        return ResponseEntity.ok("Function not implemented yet");
+    public ResponseEntity<?> readSecret(@RequestParam String secretID){        
+        SecretEntry secret = secretDAO.findById(secretID).get();
+
+        FileInputStream secretData = secret.getSecretData();
+
+        MultipartFile file = (MultipartFile) secretData;
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+        "attachment; filename=\"" + file.getName() + "\"").body(file);
     }
 
     @RequestMapping(value="/upatesecret", method = RequestMethod.POST)
-    public ResponseEntity<?> updateSecret(@RequestBody SecretEntry secret){
-        // TO DO: Implement user secret updating
-        return ResponseEntity.ok("Function not implemented yet");
+    public ResponseEntity<?> updateSecret(@RequestParam String secretID, String secretName, MultipartFile file) throws Exception {
+        SecretEntry secret = secretDAO.findById(secretID).get();
+
+        String response = "No updates made to secret";
+
+        if (secretName != "" && secretName != secret.getSecretName()) {
+            response = "Sucessfully updated secret";
+        }
+
+        if (file != null) {
+            FileInputStream secretData = (FileInputStream) file.getInputStream();
+
+            if (secretData != secret.getSecretData()) {
+                response = "Successfully updated secret";
+            }
+        }
+
+        return ResponseEntity.ok(response);
     }
 
     @RequestMapping(value="/deletesecret", method = RequestMethod.GET)
